498ff0c138a8e03dbe0b18d2617fab0eb442ca5c
Hilmy Rasyad <101026817+hlmyrsyd@users.noreply.github.com>
2025-02-06 18:52:38 +0700

    change text for fun in groud

--- 2d-portfolio/src/constants.js
+++ 2d-portfolio/src/constants.js
@@ -3,16 +3,14 @@ export const scaleFactor = 4;
 export const dialogueData = {
     pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
     I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/hlmyrsyd" target="_blank">Github</a>!`,
-    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    Here is my <a href="https://hilmyrasyad.site" target="_blank">Portfolio</a>!`,
+    "cs-degree": `This is my RevoU degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
     "sofa-table": `That's my sofa. I like to relax here and watch YouTube`,
-    tv: `That's my TV. I've been watching tech youtubers a lot recently like :ftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+    tv: `That's my TV. I've been watching RevoU Software Engineering Course here (sometimes) and here you can <a href="https://rebrand.ly/se-daftar-sini" target="_blank">Apply SE Course too</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
     resume: `This is my desk and on it is my linkedin page. <a href="https://www.linkedin.com/in/hilmyrasyad/" target="_blank">Check it out?</a>
     Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-    Kaboom.js by watching some of the jslegenddev tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    projects: `I have a lot of project recently, but If you want to ask anything, feel free to ask!`,
     library: `There are a lot of programming books on my shelves. Who else compulsively buys technical books without ever finishing them?`,
     exit: `If you want to exit hilmyrasyad's room, just close the tab.`,
 };
