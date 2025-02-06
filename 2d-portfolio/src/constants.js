d83086cf59bb28f69919c6d3cf0669d0f0cdf130
jodyboy <elite.crew66@gmail.com>
2025-02-06 19:12:21 +0700

    update for fun part

--- 2d-portfolio/src/constants.js
+++ 2d-portfolio/src/constants.js
@@ -1,7 +1,7 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    pc: `Hi, This is my PC. I work mostly in JavaScript/TypeScript these days.
     I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
     Here is my <a href="https://hilmyrasyad.site" target="_blank">Portfolio</a>!`,
     "cs-degree": `This is my RevoU degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
